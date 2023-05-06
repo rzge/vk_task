5675b935796c8e90678bc683283df034a6704209
Владислав Труханов <trukhanov.vladislav@mail.ru>
2023-05-06 15:56:05 +0300

    crispy-form -> crispy_forms

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'main',
     'users',
-    'crispy-forms',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

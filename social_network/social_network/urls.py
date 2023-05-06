684b684d29e58f9e8ff156bbd969cd3c39dff72e
Владислав Труханов <trukhanov.vladislav@mail.ru>
2023-05-06 16:11:38 +0300

    добавлен эндпоинт на регистрацию

--- social_network/social_network/urls.py
+++ social_network/social_network/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('register/', user_views.register, name='register')
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)

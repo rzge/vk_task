ccb22c07792f8eea5ddc5716d7389908cf2bb14b
Владислав Труханов <trukhanov.vladislav@mail.ru>
2023-05-08 00:27:06 +0300

    вывожу результат поиска

--- social_network/main/urls.py
+++ social_network/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('search_results/', views.search_results, name='search_results'), #перенаправит на страницу поиска
 ]
--- social_network/main/views.py
+++ social_network/main/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,3 +6,13 @@ from django.http import HttpResponse
 def index(request):
 
     return render(request, 'main/index.html')
+
+
+
+def search_results(request):
+    query = request.GET.get('query')
+    if not query:
+        query = ""
+    results = User.objects.filter(username__icontains=query)
+    print(results)
+    return render(request, 'main/search_results.html', {'results': results, 'query':query})
